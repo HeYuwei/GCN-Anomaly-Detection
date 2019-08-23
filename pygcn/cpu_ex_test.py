d07b430b63ac27e82abd124162af823d71f63baf
heyuwei <heyuwei403@gmail.com>
2019-08-23 23:50:41 +0800

    feature

--- pygcn/cpu_ex_test.py
+++ pygcn/cpu_ex_test.py
@@ -13,6 +13,6 @@ image_path = '{}/img'.format(out_full_path)
 flow_x_path = '{}/flow_x'.format(out_full_path)
 flow_y_path = '{}/flow_y'.format(out_full_path)
 
-cmd = '/mmu_ssd/liuchang03/heyuwei/dense_flow/extract_gpu -f={} -x={} -y={} -i={} -b=20 -t=1 -d={} -s=1 -o=zip'.format(vid_path, flow_x_path, flow_y_path, image_path, dev_id)
+cmd = '/mmu_ssd/liuchang03/heyuwei/dense_flow/extract_cpu -f={} -x={} -y={} -i={} -b=20 -t=1 -d={} -s=1 -o=zip'.format(vid_path, flow_x_path, flow_y_path, image_path, dev_id)
 
 os.system(cmd)
