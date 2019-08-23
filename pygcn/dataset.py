dced98db11a005fe8503c9cbc32a0bad965abe82
heyuwei <heyuwei403@gmail.com>
2019-08-23 17:09:59 +0800

    mem

--- pygcn/dataset.py
+++ pygcn/dataset.py
@@ -98,7 +98,7 @@ class UCFCrimeSlow(Dataset):
         if self.__graph_func__:
             feat = self.__graph_func__(feat, pred, uncertainty, self.__graph_param__)
         print(feat[0].shape, feat[1].shape, feat[2].shape, feat[3].shape, pred.shape)
-        print(feat.shape)
+        # print(feat.shape)
         # print('pred')
         print(vid)
         return feat, pred, vid
