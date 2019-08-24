4767a5460da8130c90107f9708bbe7e8518c8d59
heyuwei <heyuwei403@gmail.com>
2019-08-24 23:42:05 +0800

    feature

--- pygcn/checkp.py
+++ pygcn/checkp.py
@@ -1,8 +1,8 @@
-import pickle
+import os
 
-videos_pkl = 'ucf_crime_train.pkl'
-
-with open(videos_pkl, 'rb') as f:
-    videos = pickle.load(f)
-for v in videos:
-    print(v)
+mpath = '/mmu_ssd/liuchang03/heyuwei/temporal-segment-networks/3rd-party/opencv-3.4.5/modules'
+for root,dirs,files in os.walk(mpath):
+    for dir in dirs:
+        if dir == 'opencv2':
+            s_path = os.path.join(root,dir)
+            os.system('cp -r' + s_path + '/* ' + '/usr/local/include/opencv2/')
