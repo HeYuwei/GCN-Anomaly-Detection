356fbe12d8a31055f95813a2f5d96cab373d1c15
heyuwei <heyuwei403@gmail.com>
2019-08-24 23:51:15 +0800

    feature

--- pygcn/checkp.py
+++ pygcn/checkp.py
@@ -5,5 +5,6 @@ for root,dirs,files in os.walk(mpath):
     for dir in dirs:
         if dir == 'opencv2':
             s_path = os.path.join(root,dir)
-            print(s_path)
-            os.system('cp -r' + s_path + ' /usr/local/include')
+            cmd = 'cp -r' + s_path + ' /usr/local/include'
+            print(cmd)
+            os.system(cmd)
